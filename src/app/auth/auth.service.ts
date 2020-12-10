39666373cd7fb17061cfe85570af6dbe9860a980
Alejandro Ariel Cardozo Romero <ariel.card@gmail.com>
2020-12-10 14:13:10 +0100

    private

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) { }
 
   signUp(email: string, password: string){
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAcooDtAYGDff23nOPoYyUuGNuftvKwXl0',
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
       {
         email: email,
         password: password,
@@ -36,7 +36,7 @@ export class AuthService {
   }
 
   login(email: string, password: string){
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAcooDtAYGDff23nOPoYyUuGNuftvKwXl0',
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
       {
         email: email,
         password: password,
